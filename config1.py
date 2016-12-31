7012a912c41be899460111919620de0a10fd2022
Ray <hu.ray@foxmail.com>
2016-12-31 14:15:45 +0800

    fix

---
+++ config1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#
+# You must edit the config first.
+#
+
+# MySQL Config
+MYSQLConfig = {
+    'host': '',
+    'user': '',
+    'password': '',
+    'db': ''
+}
+
+
+# ZHIHUGet Config
+ZHIHUGetConfig = {
+    'USER_AGENTS': [],
+    'cookies': {},
+    'authorization': ''
+}
